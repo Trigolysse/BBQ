9c3d21982d4c3caa912ee71fe7e8333bfea01f90
Hugo SALAZAR <hugo-brock@hotmail.com>
2020-01-14 00:36:38 +0100

    Added Player

---
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AbstractClassesTool
+{
+    public abstract class Weapon
+    {
+        #region Private Fields
+
+        private int damage;
+        private WeaponName name;
+        private WeaponFireMode fireMode;
+
+        #endregion
+
+        #region Constructor
+
+        public Weapon(WeaponName name, int damage, WeaponFireMode fireMode)
+        {
+            this.Damage = damage;
+            this.Name = name;
+            this.FireMode = fireMode;
+        }
+
+        #endregion
+
+        #region Getters And Setters
+
+        public WeaponFireMode FireMode { get => fireMode; set => fireMode = value; }
+        public int Damage { get => damage; set => damage = value; }
+        public WeaponName Name { get => name; set => name = value; }
+
+        #endregion
+    }
+
+}
+
