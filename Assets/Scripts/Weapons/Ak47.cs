0ec0adc95f5238c4544dca9f33dc98c38a1f5321
Hugo SALAZAR <hugo-brock@hotmail.com>
2020-01-15 21:13:52 +0100

    Started Weapon Manager

---
+++ Assets/Scripts/Weapons/Ak47.cs
@@ -0,0 +1,31 @@
+﻿using BBQ;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ak47 : MonoBehaviour, Weapon
+{
+    private Animator animator;
+    [SerializeField]
+    private int damage;
+
+    public int Damage { get => damage; set => value = damage ; }
+
+   
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShootAnimation()
+    {
+        Debug.Log("ShootAnimation");
+        animator.SetTrigger(AnimationTags.SHOOT_TRIGGER); 
+    }
+}
--- Assets/Scripts/Weapons/Weapon.cs
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -2,36 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace AbstractClassesTool
+namespace BBQ
 {
-    public abstract class Weapon
+    public interface Weapon
     {
-        #region Private Fields
+        int Damage { get; set; }
 
-        private int damage;
-        private WeaponName name;
-        private WeaponFireMode fireMode;
-
-        #endregion
-
-        #region Constructor
-
-        public Weapon(WeaponName name, int damage, WeaponFireMode fireMode)
-        {
-            this.Damage = damage;
-            this.Name = name;
-            this.FireMode = fireMode;
-        }
-
-        #endregion
-
-        #region Getters And Setters
-
-        public WeaponFireMode FireMode { get => fireMode; set => fireMode = value; }
-        public int Damage { get => damage; set => damage = value; }
-        public WeaponName Name { get => name; set => name = value; }
-
-        #endregion
+        void ShootAnimation();
     }
 
 }
