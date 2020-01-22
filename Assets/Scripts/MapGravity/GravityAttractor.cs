fa542ce5f1a9ded1e5f35cea383c51dffae1a66b
Hugo <hugo.weiss@epita.fr>
2020-01-22 02:38:28 +0100

    Added gravity earth and capsule player

---
+++ Assets/Scripts/MapGravity/GravityAttractor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityAttractor : MonoBehaviour {
+	
+	public float gravity = -9.8f;
+	
+	
+	public void Attract(Rigidbody body) {
+		Vector3 gravityUp = (body.position - transform.position).normalized;
+		Vector3 localUp = body.transform.up;
+		
+		// Force qui attire un corps vers le bas
+		body.AddForce(gravityUp * gravity);
+		// Alligne les corps avec le centre de la planète
+		body.rotation = Quaternion.FromToRotation(localUp,gravityUp) * body.rotation;
+	}  
+}
