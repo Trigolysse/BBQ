c83d85e913512cecf9b644c8c61a7ee331de3cd5
Hugo <hugo.weiss@epita.fr>
2020-02-15 18:11:18 +0100

    Added turret rotation optimal

---
+++ Assets/TurretNRV.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class TurretNRV : MonoBehaviour
+{
+    private Transform player;
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector2.Distance(player.position, transform.position);
+        if (distance < 40)
+        {
+            Vector2 v2 = player.position - transform.position;
+            v2.Normalize();
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(v2), 1f * Time.deltaTime);
+        }
+    }
+}
