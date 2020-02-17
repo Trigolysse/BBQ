9b2838c063e045a27837f84923397d473c01d549
Hugo <hugo.weiss@epita.fr>
2020-02-17 03:10:58 +0100

    Added AI

---
+++ Assets/Deplacement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Deplacement : MonoBehaviour
+{
+    private NavMeshAgent Agentenemy;
+
+    public Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //target = GameObject.FindGameObjectsWithTag("Player").transform;
+        Agentenemy = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Agentenemy.SetDestination(target.position);
+    }
+}
