ccf20e9d0476ae4582a6bae12e0fb01a28bdb856
Hugo <hugo.weiss@epita.fr>
2020-02-17 20:48:20 +0100

    Added minimap (enabled with m)

---
+++ Assets/Affichage.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class Affichage : MonoBehaviour
+{
+    private bool Mm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mm = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            if (Mm)
+            {
+                GameObject.Find("minimapcamera").GetComponent<Camera>().enabled = true;
+                Mm = false;
+            }
+            else
+            {
+                GameObject.Find("minimapcamera").GetComponent<Camera>().enabled = false;
+                Mm = true;
+            }
+
+        }
+    }
+}
---
+++ Assets/Followplayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Followplayer : MonoBehaviour
+{
+  public float distancedujoueur;
+  public float recularriere;
+  private Vector3 Initialposition;
+
+  private Vector3 positionplayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        positionplayer = GameObject.FindGameObjectWithTag("Player").transform.position;
+        transform.position = new Vector3(positionplayer.x, positionplayer.y + distancedujoueur, positionplayer.z+recularriere);
+    }
+}
