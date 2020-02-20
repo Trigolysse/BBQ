6e49d44e14292e80934afde010509ac1f468e3fc
Hugo <hugo.weiss@epita.fr>
2020-02-20 23:55:53 +0100

    Added bullet created but still invisible

--- Assets/Tir.cs
+++ Assets/Tir.cs
@@ -24,19 +24,22 @@ public class Tir : MonoBehaviour
         {
             Vector2 Centre=new Vector2(Screen.width/2, Screen.height/2);
             ray = Camera.main.ScreenPointToRay(Centre);
-            Debug.Log("Feu");
+            //Debug.Log("Feu");
+            
+            if (Physics.Raycast(ray,out hit, Camera.main.farClipPlane))
+            {
+                //Debug.Log("tir dans l anus!!!" + hit.transform.name);
+                //hit.rigidbody.AddForceAtPosition(transform.TransformDirection(Vector3.up)*100000,hit.normal);
+            }
+
+            if (hit.transform.gameObject.tag == "Planet")
+            {
+                Debug.Log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+                GameObject Go=Instantiate(ball,hit.point,Quaternion.FromToRotation(Vector3.forward, hit.normal)) as GameObject;
+                Destroy(Go,10f);
+            }
         }
 
-        if (Physics.Raycast(ray,out hit, Camera.main.farClipPlane))
-        {
-            Debug.Log("tir dans l anus!!!" + hit.transform.name);
-            //hit.rigidbody.AddForceAtPosition(transform.TransformDirection(Vector3.up)*100000,hit.normal);
-        }
-
-        if (hit.transform.gameObject.tag == "Planet")
-        {
-            GameObject Go=Instantiate(ball,hit.point,Quaternion.FromToRotation(Vector3.forward, hit.normal)) as GameObject;
-            Destroy(Go,10f);
-        }
+       
     }
 }
