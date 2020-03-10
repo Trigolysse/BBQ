18bb9c31223bc7a37d0f93f46870b673dcde67e4
Hugo SALAZAR <hugo-brock@hotmail.com>
2020-03-10 17:09:08 +0100

    fixed crosshair

--- Assets/Crosshair.cs
+++ Assets/Crosshair.cs
@@ -13,12 +13,11 @@ public class Crosshair : MonoBehaviour
         Texture2D texture = new Texture2D(1, 1);
         texture.SetPixel(0, 0, Color.green);
         texture.Apply();
-        GUI.skin.box.normal.background = texture;
 
-        GUI.Box(new Rect(Screen.width / 2 - gap - length, Screen.height / 2 - thickness / 2, length, thickness), GUIContent.none); //Left
-        GUI.Box(new Rect(Screen.width / 2 + gap, Screen.height / 2 - thickness / 2, length, thickness), GUIContent.none); // Right
-        GUI.Box(new Rect(Screen.width / 2 - thickness / 2, Screen.height / 2 - gap - length, thickness, length), GUIContent.none); // Top
-        GUI.Box(new Rect(Screen.width / 2 - thickness / 2, Screen.height / 2 + gap, thickness, length), GUIContent.none); // Bottom
+        GUI.DrawTexture(new Rect(Screen.width / 2 - gap - length, Screen.height / 2 - thickness / 2, length, thickness), texture); //Left
+        GUI.DrawTexture(new Rect(Screen.width / 2 + gap, Screen.height / 2 - thickness / 2, length, thickness), texture); // Right
+        GUI.DrawTexture(new Rect(Screen.width / 2 - thickness / 2, Screen.height / 2 - gap - length, thickness, length), texture); // Top
+        GUI.DrawTexture(new Rect(Screen.width / 2 - thickness / 2, Screen.height / 2 + gap, thickness, length), texture); // Bottom
         
     }
 }
