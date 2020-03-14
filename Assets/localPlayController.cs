39f1872b55163b3c1e0bee6ab5edbfff25acc396
Hugo <hugo.weiss@epita.fr>
2020-03-14 13:06:23 +0100

    Added big map

---
+++ Assets/localPlayController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class localPlayController : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void naviguateBack()
+    {
+        PhotonNetwork.LoadLevel(0);
+    }
+}
