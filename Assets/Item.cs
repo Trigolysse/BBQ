23030a4117d70ee350f919dde6f0c43dc56aa4a1
Hugo <hugo.weiss@epita.fr>
2020-04-11 18:16:09 +0200

    Commit inventaire aseet

--- Assets/Item.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-[CreateAssetMenu(fileName ="Nouvel objet", menuName ="Inventaire/Créer un objet")]
-public class Item : ScriptableObject
-{
-    public new string name;
-    public Sprite icon;
-    public string description;
-}
