b2a3e71166ec53196a4a7b8ee19ca6ccc9ef6c61
mrkeinzi <59828978+mrkeinzi@users.noreply.github.com>
2020-05-15 15:41:55 +0200

    Added Sniper Scope

---
+++ Assets/Scripts/Weapons/SniperScript.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SniperScript : MonoBehaviour
+{
+
+    public GameObject playerCam;
+    public GameObject SniperScope;
+
+
+    private void Start()
+    {
+        SniperScope.SetActive(false);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            playerCam.GetComponent<Camera>().fieldOfView = 16.6f;
+            SniperScope.SetActive(true);
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            playerCam.GetComponent<Camera>().fieldOfView = 68;
+            SniperScope.SetActive(false);
+        }
+
+    }
+}
