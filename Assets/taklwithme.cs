af2039bd79b0cb46867532f889bd1ba9410259dd
Hugo <hugo.weiss@epita.fr>
2020-05-18 17:47:32 +0200

    Added PNJ

---
+++ Assets/taklwithme.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class taklwithme : MonoBehaviour
+{
+    public Dialogue dialogue;
+    public GameObject talk;
+    private bool begin = false;
+    public GameObject dialogmanager;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        talk.SetActive(true);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (begin==false)
+            {
+                talk.SetActive(false);
+                            
+                FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+                begin = true;
+            }
+            else
+            {
+                Debug.Log("Vive Kaaris");
+                dialogmanager.GetComponent<DialogueManager>().DisplayNextSentence();
+                
+            }
+            
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        talk.SetActive(false);
+        begin = false;
+        dialogmanager.GetComponent<DialogueManager>().EndDialogue();
+    }
+}
