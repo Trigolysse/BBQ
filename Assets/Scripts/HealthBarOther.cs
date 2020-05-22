22629f9d74e3b7b6f6b1dda45ecbbb2e5924801d
mrkeinzi <59828978+mrkeinzi@users.noreply.github.com>
2020-05-22 16:26:48 +0200

    HealthBarEnnemy

---
+++ Assets/Scripts/HealthBarOther.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class HealthBarOther : MonoBehaviourPunCallbacks
+{
+    private Camera Main;
+    private GameObject[] playerList;
+    
+
+    private void Start()
+    {
+        playerList = GameObject.FindGameObjectsWithTag("Player");
+    }
+    private void Update()
+    {
+        foreach (GameObject player in playerList)
+        {
+            if (player.PhotonView.IsMine)
+            {
+                Main = player.GetComponent<Camera>().MainCamera;
+                transform.LookAt(transform.position + Main.forward);
+            }
+        }
+    }
+}
