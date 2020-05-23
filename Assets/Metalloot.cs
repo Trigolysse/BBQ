417fb7d4eb3f0470971f03cac5f5d07a4d81b626
Arrytrax <60196150+Arrytrax@users.noreply.github.com>
2020-05-23 18:19:33 +0200

    Added allshit

---
+++ Assets/Metalloot.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Metalloot : MonoBehaviour
+{
+    public int zone;
+    private GameObject gamemanager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gamemanager = GameObject.FindWithTag("Gamemanager");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+
+        Debug.Log("FEFEW");
+        if (other.CompareTag("Player"))
+        {
+            gamemanager.GetComponent<Metalspawn>().destroyflower(zone);
+            //var playerInventory = other.gameObject.GetComponent<Inventory>();
+            //if(playerInventory != null)
+            //playerInventory.AddInInventory(new Stack(new Item(0, "flower", flowerSprite, ItemType.DIRT), 1));
+            PhotonNetwork.Destroy(this.gameObject);
+        }
+
+
+    }
+}
