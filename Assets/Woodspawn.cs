0f3dae4dca673519a40737df7f5b43aa93f123ba
Arrytrax <60196150+Arrytrax@users.noreply.github.com>
2020-05-24 13:58:03 +0200

    Added fixbug

--- Assets/Woodspawn.cs
+++ Assets/Woodspawn.cs
@@ -39,27 +39,31 @@ public class Woodspawn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Random rand = new Random();
-        for (int i = 0; i < alive.Count; i++)
+        if (PhotonNetwork.IsMasterClient)
         {
-            if (!alive[i])
+            Random rand = new Random();
+            for (int i = 0; i < alive.Count; i++)
             {
-                restime[i] += Time.deltaTime;
-            }
-            if (restime[i] >= respawntime)
-            {
-                restime[i] = 0;
-                alive[i] = true;
-                if (PhotonNetwork.IsMasterClient)
+                if (!alive[i])
                 {
-                    GameObject newflower = PhotonNetwork.InstantiateSceneObject("Wood", allspwan[i][rand.Next(allspwan[i].Length)], Quaternion.identity) as GameObject;
-                    newflower.GetComponent<Woodloot>().zone = i;
+                    restime[i] += Time.deltaTime;
+                }
+                if (restime[i] >= respawntime)
+                {
+                    restime[i] = 0;
+                    alive[i] = true;
+                    if (PhotonNetwork.IsMasterClient)
+                    {
+                        GameObject newflower = PhotonNetwork.InstantiateSceneObject("Wood", allspwan[i][rand.Next(allspwan[i].Length)], Quaternion.identity) as GameObject;
+                        newflower.GetComponent<Woodloot>().zone = i;
+                    }
                 }
             }
         }
     }
     public void destroyflower(int zone)
     {
-        alive[zone] = false;
+        if (PhotonNetwork.IsMasterClient)
+            alive[zone] = false;
     }
 }
