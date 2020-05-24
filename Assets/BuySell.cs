b09aab11020d5b8629cf894b9c1602387f003dc6
Hugo <hugo.weiss@epita.fr>
2020-05-24 18:59:09 +0200

    ulisse le boxeur

---
+++ Assets/BuySell.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class BuySell : MonoBehaviourPunCallbacks
+{
+    
+    public GameObject BuySelll;
+    private bool droit;
+    public GameObject canvassell;
+    private Player player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player=gameObject.GetComponent<Player>();
+        BuySelll.SetActive(false);
+        droit = true;
+        canvassell.SetActive(false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag=="PNJ")
+        {
+            if (photonView.IsMine)
+            {
+                BuySelll.SetActive(true);
+            }
+            
+        }
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (droit && Input.GetKeyUp(KeyCode.F))
+        {
+            BuySelll.SetActive(false);
+            droit = false;
+        }
+        if (droit && Input.GetKeyUp(KeyCode.O))
+        {
+            BuySelll.SetActive(false);
+            canvassell.SetActive(true);
+            
+        }
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag=="PNJ")
+        {
+            
+            BuySelll.SetActive(false);
+            canvassell.SetActive(false);
+        }
+
+        droit = true;
+    }
+}
--- Assets/SimpleInv.cs
+++ Assets/SimpleInv.cs
@@ -21,23 +21,16 @@ public class SimpleInv : MonoBehaviourPunCallbacks
     public GameObject Sword;
     public Image SwordImage;
     public Image AKImage;
-    public GameObject BuySell;
-    private bool droit;
-    public GameObject canvassell;
     private Player player;
+    
 
     public int[] count = new int[6];
     // Start is called before the first frame update
     void Start()
     {
-        
-        {
-            
-        }
         player=gameObject.GetComponent<Player>();
-        
         inventaire.SetActive(false);
-        BuySell.SetActive(false);
+        
         for(int i = 0;i <6;i++)
         {
             lamoula[i].text = "0";
@@ -45,8 +38,7 @@ public class SimpleInv : MonoBehaviourPunCallbacks
         }
 
         ouvert = false;
-        droit = true;
-        canvassell.SetActive(false);
+        
     }
 
     // Update is called once per frame
@@ -105,43 +97,5 @@ public class SimpleInv : MonoBehaviourPunCallbacks
     }
 
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.tag=="PNJ")
-        {
-            if (photonView.IsMine)
-            {
-                BuySell.SetActive(true);
-            }
-            
-        }
-    }
-
-    public void OnTriggerStay(Collider other)
-    {
-        if (droit && Input.GetKeyUp(KeyCode.F))
-        {
-            BuySell.SetActive(false);
-            droit = false;
-        }
-        if (droit && Input.GetKeyUp(KeyCode.O))
-        {
-            BuySell.SetActive(false);
-            canvassell.SetActive(true);
-            
-        }
-        
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.tag=="PNJ")
-        {
-            
-                BuySell.SetActive(false);
-                canvassell.SetActive(false);
-        }
-
-        droit = true;
-    }
+   
 }
