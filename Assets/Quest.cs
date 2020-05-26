3f75290e6dc4695ba050e6aea58572af9053e066
Arrytrax <60196150+Arrytrax@users.noreply.github.com>
2020-05-26 15:59:17 +0200

    Added Questforeveryone

--- Assets/Quest.cs
+++ Assets/Quest.cs
@@ -17,10 +17,21 @@ public class Quest : MonoBehaviour
     public Canvas[] Rewarddis;
     public Button complete;
     private GameObject gamemanager;
+    public Canvas alldisp;
+    public bool cancomplete;
     // Start is called before the first frame update
     void Start()
     {
         gamemanager = GameObject.FindWithTag("Gamemanager");
+        alldisp.enabled = false;
+        for (int i = 0; i < 10; i++)
+        {
+             Goaldis[i].enabled = false;
+        }
+        for (int i = 0; i < 4; i++)
+        {
+             Rewarddis[i].enabled = false;
+        }
     }
 
     // Update is called once per frame
@@ -28,6 +39,25 @@ public class Quest : MonoBehaviour
     {
         Debug.Log(Goal);
         Debug.Log(Reward);
+        if(Input.GetKeyUp(KeyCode.P))
+        {
+            alldisp.enabled = !alldisp.enabled;
+        }
+
+        for(int i = 0; i <10; i++)
+        {
+            if (i == Goal)
+                Goaldis[i].enabled = true;
+            else
+                Goaldis[i].enabled = false;
+        }
+        for (int i = 0; i < 4; i++)
+        {
+            if (i == Reward)
+                Rewarddis[i].enabled = true;
+            else
+                Rewarddis[i].enabled = false;
+        }
     }
 
 
@@ -35,4 +65,21 @@ public class Quest : MonoBehaviour
     {
 
     }
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "PNJ")
+        {
+            cancomplete = true;
+        }
+
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "PNJ")
+        {
+            cancomplete = false;
+        }
+
+    }
+
 }
